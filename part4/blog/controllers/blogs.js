c215a9a5ff338f410c22492c6f7a1fd6bf1af263
Elias Peltokangas <elias.peltokangas@gmail.com>
2025-01-22 12:49:52 +0200

    4.17

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
   
@@ -14,7 +15,12 @@ if(blog.title === undefined || blog.url === undefined) {
     return
 }
 
+const user = await User.findById('6790c83902ec085d4450bfbf') // hardcoded user id
+blog.user = user._id
+
 const savedBlog = await blog.save()
+user.blogs = user.blogs.concat(savedBlog._id)
+await user.save()
 response.status(201).json(savedBlog)
 })
 
