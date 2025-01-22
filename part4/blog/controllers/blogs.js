f184d3abbd099eed6cc80d94f885d0d38a568f86
Elias Peltokangas <elias.peltokangas@gmail.com>
2025-01-22 14:46:50 +0200

    4.19

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,21 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
   
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
 const blog = new Blog(request.body)
 
 if(blog.title === undefined || blog.url === undefined) {
-    response.status(400).end()
-    return
+  response.status(400).end()
+  return
 }
 
-const user = await User.findById('6790c83902ec085d4450bfbf') // hardcoded user id
+const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+if(!decodedToken.id) {
+  return response.status(401).json({ error: 'token missing or invalid' })
+}
+
+const user = await User.findById(decodedToken.id)
 blog.user = user._id
 
 const savedBlog = await blog.save()
