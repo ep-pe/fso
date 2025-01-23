c6e3f83609267ac1fcc0953f5cb9def2aaeca831
Elias Peltokangas <elias.peltokangas@gmail.com>
2025-01-23 19:29:53 +0200

    up to 4.7

--- part5/blog_frontend/src/components/Blog.jsx
+++ part5/blog_frontend/src/components/Blog.jsx
@@ -1,7 +1,23 @@
-const Blog = ({ blog }) => (
-  <div className="blog">
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false)
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded)
+  }
+
+  return (
+      <div className="blog">
+        {blog.title} {blog.author} <button onClick={() => toggleExpanded()}>{expanded ? 'hide' : 'view'}</button>
+        <div className="blog" style={showWhenExpanded}>
+          <div>{blog.url}</div>
+          <div>{blog.likes} likes <button>like</button></div>
+          <div>{blog.user ? blog.user.name : ''}</div>
+        </div>
+      </div>
+  )  
+}
 
 export default Blog
